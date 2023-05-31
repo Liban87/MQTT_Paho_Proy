33ad8dc44981aaffac400df127f1178016b91864
Anderson Ruales <97903987+aarualest@users.noreply.github.com>
2023-05-30 19:50:25 -0500

    Add files via upload

---
+++ MqttPub.py
@@ -0,0 +1,66 @@
+import paho.mqtt.client as mqtt
+import pandas as pd
+import json
+import joblib
+import time
+from sklearn.ensemble import RandomForestClassifier
+import xgboost
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+
+def send_dataframe_rows_broker(broker_address, topic, file_path, model_path):
+    # Cargar el archivo xlsx como un DataFrame
+    df = pd.read_excel(file_path)
+
+    # Cargar el modelo desde el archivo .pkl
+    model = joblib.load(model_path)
+    model = model[1]
+
+    # Crear un cliente MQTT
+    client = mqtt.Client()
+
+    # Conectar al broker MQTT
+    client.connect(broker_address)
+
+    # Iterar sobre las filas del DataFrame
+    d = {0:'Sin Consumo', 1:'Zona 2', 2:'Zona 3', 3:'Igual' }
+    
+    for _, row in df.iterrows():
+        # Obtener las características de la fila como una matriz NumPy
+        features = pd.DataFrame(row.values.reshape(1, -1), columns = df.columns.tolist())
+
+        # Realizar la predicción con el modelo cargado
+        prediction = model.predict(features)
+        ind = np.argmax(prediction)
+        prediction = d[ind]        
+        probability = model.predict_proba(features)[0] #.max()
+
+        # Crear un diccionario con los resultados
+        result = {
+            'prediction': prediction,
+            'probability': probability.tolist()
+        }
+
+        # Convertir el diccionario en una cadena JSON
+        payload = json.dumps(result)
+
+        # Publicar los resultados en el topic especificado
+        client.publish(topic, payload)
+
+        # Esperar 1 minuto
+        time.sleep(5)
+
+    # Desconectar del broker MQTT
+    #client.disconnect()
+
+broker_address = "test.mosquitto.org"
+topic = "ALSW/Prediccion"
+file_path = "C:/Users/ander/Desktop/UAM/Doctorado/Industria 4.0/Final/produccion.xlsx"
+model_path = "C:/Users/ander/Desktop/UAM/Doctorado/Industria 4.0/Final/Model.pkl"
+
+send_dataframe_rows_broker(broker_address, topic, file_path, model_path)
+
+
+#client = mqtt.Client()
+#client.connect("test.mosquitto.org", 1883, 60)
+#client.publish("ALSW/temp", "99")
