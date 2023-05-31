ea8a26ea9bf33fb5ddb0d35a7d28483b277fb975
Ivan Arias <lialbertoarias@gmail.com>
2023-05-30 21:16:06 -0500

    Now runing an example that publishes in the bulb1 topic

--- MQTT_Paho_client.py
+++ MQTT_Paho_client.py
@@ -14,17 +14,24 @@ def on_connect(client, userdata, flags, rc):
 
 mqtt.Client.connected_flag=False#create flag in class
 broker="127.0.0.1"
+port=1883
 client = mqtt.Client("python1")             #create new instance
-client.username_pw_set('Ivan',password = 'alpamayo')
+client.username_pw_set('Ivan',password = 'alpamayo') #configure user and password for MQTT broker connection
 client.on_connect=on_connect  #bind call back function
+def on_publish(client,userdata,result):             #create function for callback
+    print("data published \n")
+    pass
+client.on_publish = on_publish                         #assign function to callback
 client.loop_start()
 print("Connecting to broker ",broker)
-client.connect(broker)      #connect to broker
+client.connect(broker,port)      #connect to broker
 n_atmp=0
-while not client.connected_flag and n_atmp<10: #wait in loop
+while not client.connected_flag and n_atmp<10: #wait in loop while not connected
     print("In wait loop ",n_atmp," attempt")
     time.sleep(1)
     n_atmp+=1
 print("in Main Loop")
+ret= client.publish("bulb1","on") #publish
+
 client.loop_stop()    #Stop loop 
 client.disconnect() # disconnect
