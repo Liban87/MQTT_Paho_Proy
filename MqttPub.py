c70e39ebf51ddd745b4c595cc95860d2849cdbbf
Anderson Ruales <97903987+aarualest@users.noreply.github.com>
2023-05-31 21:20:01 -0500

    Variables completas

--- MqttPub.py
+++ MqttPub.py
@@ -8,7 +8,7 @@ import xgboost
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 
-def send_dataframe_rows_broker(broker_address, port, topic, file_path, model_path):
+def send_dataframe_rows_broker(broker_address, topic, file_path, model_path):
     # Cargar el archivo xlsx como un DataFrame
     df = pd.read_excel(file_path)
 
@@ -17,12 +17,10 @@ def send_dataframe_rows_broker(broker_address, port, topic, file_path, model_pat
     model = model[1]
 
     # Crear un cliente MQTT
-    client = mqtt.Client("Python1")
-
-    client.username_pw_set('Ivan',password = 'alpamayo') #configure user and password for MQTT broker connection
+    client = mqtt.Client()
 
     # Conectar al broker MQTT
-    client.connect(broker_address,port)
+    client.connect(broker_address)
 
     # Iterar sobre las filas del DataFrame
     d = {0:'Sin Consumo', 1:'Zona 2', 2:'Zona 3', 3:'Igual' }
@@ -39,6 +37,10 @@ def send_dataframe_rows_broker(broker_address, port, topic, file_path, model_pat
 
         # Crear un diccionario con los resultados
         result = {
+            'PotAct': features.loc[0, 'Global_active_power'],
+            'PotReact': features.loc[0, 'Global_reactive_power'],
+            'Volt': features.loc[0, 'Voltage'],
+            'Int': features.loc[0, 'Global_intensity'],
             'prediction': prediction,
             'probability': probability.tolist()
         }
@@ -50,20 +52,17 @@ def send_dataframe_rows_broker(broker_address, port, topic, file_path, model_pat
         client.publish(topic, payload)
 
         # Esperar 1 minuto
-        time.sleep(20)
+        time.sleep(5)
 
-    #Desconectar del broker MQTT
-    client.disconnect()
+    # Desconectar del broker MQTT
+    #client.disconnect()
 
-broker_address = "127.0.0.1"
-port=1883
-topic = "Prediccion"
-file_path ="./Data/produccion.xlsx"
-model_path ="./Modelo/Model.pkl"
-#file_path = "C:/Users/ander/Desktop/UAM/Doctorado/Industria 4.0/Final/produccion.xlsx"
-#model_path = "C:/Users/ander/Desktop/UAM/Doctorado/Industria 4.0/Final/Model.pkl"
+broker_address = "test.mosquitto.org"
+topic = "ALSW/Prediccion"
+file_path = "C:/Users/ander/Desktop/UAM/Doctorado/Industria 4.0/Final/Data/produccion.xlsx"
+model_path = "C:/Users/ander/Desktop/UAM/Doctorado/Industria 4.0/Final/Modelo/Model.pkl"
 
-send_dataframe_rows_broker(broker_address, port, topic, file_path, model_path)
+send_dataframe_rows_broker(broker_address, topic, file_path, model_path)
 
 
 #client = mqtt.Client()
